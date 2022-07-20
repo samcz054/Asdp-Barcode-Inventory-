9389c8383208ddb3fed7fcc44c3afee3ff4914d8
samCZ054 <aramadhan39@gmail.com>
2022-07-20 16:48:51 +0700

    seeder

---
+++ database/seeds/CreateAdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Rating;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        // User::create([
+        //     'name'=>'admin1',
+        //     'email'=>'admin1@test.com',
+        //     'role'=>'admin',
+        //     'password'=>bcrypt('12345678'),
+        // ]);
+
+    }
+}
